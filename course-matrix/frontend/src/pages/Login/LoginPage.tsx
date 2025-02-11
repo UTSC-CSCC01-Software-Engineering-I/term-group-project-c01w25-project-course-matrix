4e348eb1d988132dc0a343416cd7990406c2178a
Masahisa Sekita <masahisa.sekita@mail.utoronto.ca>
2025-02-11 15:56:34 -0500

    git commit -m "can't quite center the card for some reason"

--- course-matrix/frontend/src/pages/Login/LoginPage.tsx
+++ course-matrix/frontend/src/pages/Login/LoginPage.tsx
@@ -4,21 +4,20 @@ import { Input } from "@/components/ui/input"
 import { Label } from "@/components/ui/label"
 
 const LoginPage = () => {
-  return <div className="flex justify-center items-center h-screen">
+  return <div
+  className="flex justify-center items-center h-screen">
     <Card className="w-[350px] p-8">
       <div className="flex flex-col gap-4">
         <h1 className="text-xl font-bold">Please Login or Signup</h1>
         <h2 className="text-sm text-slate-500">If you have an account please log in, if you do not have an account please sign up.</h2>
-        <Label htmlFor="name">Username</Label>
-        {/* <h2 className="text-sm text-slate-500">Username</h2> */}
-        <Input placeholder="Input user name"/>
-        {/* <h2 className="text-sm text-slate-500">Password</h2> */}
-        <Label htmlFor="name">Password</Label>
+        <Label htmlFor="Username">Username</Label>
+        <Input id="UsernameInput" placeholder="Input user name"/>
+        <Label id="PasswordInput"htmlFor="Password">Password</Label>
         <Input placeholder="input user password"/>
         <div className="w-full flex flex-row justify-between">
-          <Button variant="outline">Login</Button>
+          <Button id="LoginBtn">Login</Button>
           <h2 className="text-xl font-bold">Or</h2>
-          <Button variant="outline">Sign Up</Button>
+          <Button id="SigningUpbtn">Sign Up</Button>
         </div>
       </div>
     </Card>

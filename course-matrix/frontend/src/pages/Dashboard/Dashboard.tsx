16c3993f97da916d65e5bb6a20d0b95d1ced331f
Austin-X <xaustin03@gmail.com>
2025-02-14 12:36:32 -0500

    Add User Menu Dropdown Component (#27)
    Co-authored-by: Kevin Lan <kevinlan416@gmail.com>

--- course-matrix/frontend/src/pages/Dashboard/Dashboard.tsx
+++ course-matrix/frontend/src/pages/Dashboard/Dashboard.tsx
@@ -1,6 +1,7 @@
 import { AppSidebar } from "@/components/app-sidebar"
 import { Breadcrumb, BreadcrumbList, BreadcrumbItem, BreadcrumbLink, BreadcrumbSeparator, BreadcrumbPage } from "@/components/ui/breadcrumb"
 import { SidebarProvider, SidebarInset, SidebarTrigger } from "@/components/ui/sidebar"
+import { UserMenu } from "@/components/UserMenu"
 import { Separator } from "@radix-ui/react-separator"
 import { Link, Navigate, Route, Routes, useLocation } from "react-router-dom"
 import TimetableBuilder from "../TimetableBuilder/TimetableBuilder"
@@ -14,39 +15,42 @@ const Dashboard = () => {
         <SidebarProvider>
           <AppSidebar />
           <SidebarInset>
-            <header className="flex h-16 shrink-0 items-center gap-2 border-b px-4">
-              <SidebarTrigger className="-ml-1" />
-              <Separator orientation="vertical" className="mr-2 h-4" />
-              <Breadcrumb>
-                <BreadcrumbList>
-                  <BreadcrumbItem className="hidden md:block">
-                    {location.pathname === "/dashboard/home" ? 
-                    <BreadcrumbLink href="/dashboard/home">
-                      <Link to="/dashboard/home" >
-                        Home
-                      </Link>
-                    </BreadcrumbLink> : 
-                    location.pathname === "/dashboard/timetable" ?
-                    <BreadcrumbLink href="/dashboard/timetable">
-                      <Link to="/dashboard/timetable" >
-                        Timetable Builder
-                      </Link>
-                    </BreadcrumbLink> :
-                    location.pathname === "/dashboard/assistant" ?
-                    <BreadcrumbLink href="/dashboard/assistant">
-                      <Link to="/dashboard/assistant" >
-                        AI Assistant
-                      </Link>
-                    </BreadcrumbLink>
-                    : <></>
-                    }
-                  </BreadcrumbItem>
-                  {/* <BreadcrumbSeparator className="hidden md:block" /> */}
-                  {/* <BreadcrumbItem>
-                    <BreadcrumbPage>Data Fetching</BreadcrumbPage>
-                  </BreadcrumbItem> */}
-                </BreadcrumbList>
-              </Breadcrumb>
+            <header className="flex justify-between h-16 shrink-0 items-center gap-2 border-b px-4">
+              <div className="flex flex-row items-center gap-2">
+                <SidebarTrigger className="-ml-1" />
+                <Separator orientation="vertical" className="mr-2 h-4" />
+                <Breadcrumb>
+                  <BreadcrumbList>
+                    <BreadcrumbItem className="hidden md:block">
+                      {location.pathname === "/dashboard/home" ? 
+                      <BreadcrumbLink href="/dashboard/home">
+                        <Link to="/dashboard/home" >
+                          Home
+                        </Link>
+                      </BreadcrumbLink> : 
+                      location.pathname === "/dashboard/timetable" ?
+                      <BreadcrumbLink href="/dashboard/timetable">
+                        <Link to="/dashboard/timetable" >
+                          Timetable Builder
+                        </Link>
+                      </BreadcrumbLink> :
+                      location.pathname === "/dashboard/assistant" ?
+                      <BreadcrumbLink href="/dashboard/assistant">
+                        <Link to="/dashboard/assistant" >
+                          AI Assistant
+                        </Link>
+                      </BreadcrumbLink>
+                      : <></>
+                      }
+                    </BreadcrumbItem>
+                    {/* <BreadcrumbSeparator className="hidden md:block" /> */}
+                    {/* <BreadcrumbItem>
+                      <BreadcrumbPage>Data Fetching</BreadcrumbPage>
+                    </BreadcrumbItem> */}
+                  </BreadcrumbList>
+                </Breadcrumb>
+              </div>
+              <UserMenu />
             </header>
             <div>
               <Routes>

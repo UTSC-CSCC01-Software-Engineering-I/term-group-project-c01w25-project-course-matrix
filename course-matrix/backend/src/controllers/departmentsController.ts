b94deb234bee8e1ee392df70301e6764a83ed998
dawangk <85403980+dawangk@users.noreply.github.com>
2025-02-14 12:36:09 -0500

    Ty/scrum 80 user auth backend (#26)
    Co-authored-by: Kevin Lan <kevinlan416@gmail.com>
    Co-authored-by: Austin-X <xaustin03@gmail.com>

--- course-matrix/backend/src/controllers/departmentsController.ts
+++ course-matrix/backend/src/controllers/departmentsController.ts
@@ -1,24 +1,26 @@
-import { Request, Response } from "express";
-import asyncHandler from "../middleware/asyncHandler";
-import { supabaseCourseClient } from "../db/setupDb";
+import {Request, Response} from 'express';
+
+import {supabaseCourseClient} from '../db/setupDb';
+import asyncHandler from '../middleware/asyncHandler';
 
 export default {
-    getDepartments: asyncHandler(async (req: Request, res: Response) => {
-        try {
-            // Query the departments table from the database
-            let departmentsQuery = supabaseCourseClient.from("departments").select();
+  getDepartments: asyncHandler(async (req: Request, res: Response) => {
+    try {
+      // Query the departments table from the database
+      let departmentsQuery = supabaseCourseClient.from('departments').select();
 
-            // Get the data and errors from the query
-            const { data: departmentsData, error: departmentsError } = await departmentsQuery;
+      // Get the data and errors from the query
+      const {data: departmentsData, error: departmentsError} =
+          await departmentsQuery;
 
-            // Set the departments data
-            const departments = departmentsData || [];
+      // Set the departments data
+      const departments = departmentsData || [];
 
-            // Return the departments
-            res.status(200).json(departments);
-        } catch (error) {
-            console.error(error);
-            res.status(500).json({ message: "Internal Server Error" });
-        }
-    }),
+      // Return the departments
+      res.status(200).json(departments);
+    } catch (error) {
+      console.error(error);
+      res.status(500).json({message: 'Internal Server Error'});
+    }
+  }),
 }
--- course-matrix/backend/src/db/setupDb.ts
+++ course-matrix/backend/src/db/setupDb.ts
@@ -1,8 +1,9 @@
-import { createClient } from '@supabase/supabase-js'
-import config from "../config/config";
+import {createClient} from '@supabase/supabase-js'
+
+import config from '../config/config';
 
 export const supabase = createClient(config.DATABASE_URL!, config.DATABASE_KEY!);
 
-export const supabaseCourseClient = createClient(config.DATABASE_URL!, config.DATABASE_KEY!, { db: { schema: "course" } });
+export const supabaseCourseClient = createClient(config.DATABASE_URL!, config.DATABASE_KEY!, {db: {schema: 'course'}});
 
-console.log("Connected to Supabase Client!")
+console.log('Connected to Supabase Client!')

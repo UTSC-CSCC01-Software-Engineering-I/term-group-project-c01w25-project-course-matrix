324010d7eadd7b80f7a025a88a9b2ac63b638374
dawangk <thomasyzy7@gmail.com>
2025-02-16 11:24:14 -0500

    Added 2 endpoints for password reset and added comments to the code

--- course-matrix/backend/src/routes/authRouter.ts
+++ course-matrix/backend/src/routes/authRouter.ts
@@ -1,7 +1,7 @@
 import express from 'express';
 
 import {handleAuthCode} from '../controllers/authentication'
-import {login, logout, session, signUp} from '../controllers/userController'
+import {login, logout, session, signUp, requestPasswordReset, resetPassword} from '../controllers/userController'
 
 export const authRouter = express.Router();
 
@@ -10,3 +10,5 @@ authRouter.post('/login', login);
 authRouter.post('/logout', logout);
 authRouter.get('/confirm', handleAuthCode);
 authRouter.get('/session', session);
+authRouter.post('/request-password-reset', requestPasswordReset);
+authRouter.post('/reset-password', resetPassword);

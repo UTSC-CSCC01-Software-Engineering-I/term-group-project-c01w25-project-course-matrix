a4daf6f75a1e5eeb772b25cabd18aa0a010340a6
FLAME <xaustin03@gmail.com>
2025-02-16 21:12:51 -0500

    Test workflow

--- course-matrix/frontend/src/App.tsx
+++ course-matrix/frontend/src/App.tsx
@@ -10,7 +10,7 @@ function App() {
   return (
     <div className="w-full">
       <Routes>
-        <Route path="*" element={<Navigate to="/login" />} />
+            <Route path="*" element={<Navigate to="/login" />} />
         <Route path="/" element={<Navigate to="/login" />} />
         <Route path="signup-success" element={<SignupSuccessfulPage />} />
         <Route path="/login" element={<LoginPage />} />

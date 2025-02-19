2661dda2bf3d908448ad61c54f615f0dd220625d
Kevin Lan <kevinlan416@gmail.com>
2025-02-18 21:23:46 -0500

    Fix: Password max lenght validation

--- course-matrix/frontend/src/models/signup-form.ts
+++ course-matrix/frontend/src/models/signup-form.ts
@@ -10,6 +10,7 @@ export const SignupFormSchema: ZodType<SignupForm> = z.object({
   email: z.string().min(1, "Please enter an email").email("Invalid email"),
   password: z.string()  
     .min(8, "Password must be at least 8 characters")
+    .max(50, "Password must be 50 characters or less")
     .regex(/[A-Z]/, "Password must contain at least one uppercase letter")
     .regex(/[a-z]/, "Password must contain at least one lowercase letter")
     .regex(/[0-9]/, "Password must contain at least one number")

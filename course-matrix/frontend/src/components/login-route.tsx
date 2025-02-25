f009350fb1c21b4a37e9084e268498b5505dae74
Kevin Lan <kevinlan416@gmail.com>
2025-02-24 19:37:57 -0500

    Fix: Login infinite loop bug

--- course-matrix/frontend/src/components/login-route.tsx
+++ course-matrix/frontend/src/components/login-route.tsx
@@ -12,9 +12,9 @@ interface AuthRouteProps {
  * Checks if a user session exists in localstorage. If so then redirect to dashboard.
  */
 const LoginRoute: React.FC<AuthRouteProps> = ({ component: Component }) => {
-  const user = localStorage.getItem("userInfo");
+  const { data, isLoading, error } = useGetSessionQuery();
 
-  return user ? <Navigate to="/dashboard" replace /> : <Component />;
+  return data?.user ? <Navigate to="/dashboard" replace /> : <Component />;
 };
 
 export default LoginRoute;

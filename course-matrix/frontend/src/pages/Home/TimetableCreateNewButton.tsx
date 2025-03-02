39cbc23701ab6bdb731fba1bcd042a7f246baaa1
Austin-X <Austin-X@users.noreply.github.com>
2025-03-02 06:59:48 +0000

    Auto-formatted the code using Prettier

--- course-matrix/frontend/src/pages/Home/TimetableCreateNewButton.tsx
+++ course-matrix/frontend/src/pages/Home/TimetableCreateNewButton.tsx
@@ -3,12 +3,12 @@ import { Plus } from "lucide-react";
 import { Link } from "react-router-dom";
 
 const TimetableCreateNewButton = () => (
-	<Link to="/dashboard/timetable">
-		<Button size="sm" className="px-5 bg-black hover:bg-gray-700">
-			Create New
-			<Plus />
-		</Button>
-	</Link>
+  <Link to="/dashboard/timetable">
+    <Button size="sm" className="px-5 bg-black hover:bg-gray-700">
+      Create New
+      <Plus />
+    </Button>
+  </Link>
 );
 
 export default TimetableCreateNewButton;
--- course-matrix/frontend/src/pages/TimetableBuilder/TimetableBuilder.tsx
+++ course-matrix/frontend/src/pages/TimetableBuilder/TimetableBuilder.tsx
@@ -34,8 +34,8 @@ import { FilterForm, FilterFormSchema } from "@/models/filter-form";
 import { useGetCoursesQuery } from "@/api/coursesApiSlice";
 import { useDebounceValue } from "@/utils/useDebounce";
 import SearchFilters from "./SearchFilters";
-import Calendar from 'react-calendar';
-import 'react-calendar/dist/Calendar.css';
+import Calendar from "react-calendar";
+import "react-calendar/dist/Calendar.css";
 
 type ValuePiece = Date | null;
 type Value = ValuePiece | [ValuePiece, ValuePiece];

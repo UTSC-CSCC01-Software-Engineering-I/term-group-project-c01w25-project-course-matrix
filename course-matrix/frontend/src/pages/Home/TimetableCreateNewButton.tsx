ecba887db7161bad1a0a6900c9caf7ff3ec3b8e7
Austin-X <xaustin03@gmail.com>
2025-03-02 21:53:40 -0500

    Update course-matrix/frontend/src/pages/Home/TimetableCreateNewButton.tsx
    Co-authored-by: Kevin Lan <kevinlan416@gmail.com>

--- course-matrix/frontend/src/pages/Home/TimetableCreateNewButton.tsx
+++ course-matrix/frontend/src/pages/Home/TimetableCreateNewButton.tsx
@@ -8,7 +8,7 @@ import { Link } from "react-router-dom";
  */
 const TimetableCreateNewButton = () => (
   <Link to="/dashboard/timetable">
-    <Button size="sm" className="px-5 bg-black hover:bg-gray-700">
+    <Button size="sm" className="px-5">
       Create New
       <Plus />
     </Button>

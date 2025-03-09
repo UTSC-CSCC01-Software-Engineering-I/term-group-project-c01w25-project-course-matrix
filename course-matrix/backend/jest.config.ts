dba90cc3490cf4331575c5fbda5e898f19cc2079
FLAME <xaustin03@gmail.com>
2025-03-08 23:08:29 -0500

    Fix module linting errors

--- course-matrix/backend/jest.config.ts
+++ course-matrix/backend/jest.config.ts
@@ -1,25 +1,21 @@
 import type { Config } from "jest";
 
 const config: Config = {
-  preset: "ts-jest",
-  moduleNameMapper: {
-    "\\.(css|scss)$": "identity-obj-proxy",
-    "^.+\\.svg": "<rootDir>/tests/mocks/svgMock.tsx",
-  },
-  // to obtain access to the matchers.
-  setupFilesAfterEnv: ["<rootDir>/tests/setupTests.ts"],
-  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-  modulePaths: ["<rootDir>"],
-  testEnvironment: "jsdom",
-  transform: {
-    "^.+\\.(ts|tsx)$": [
-      "ts-jest",
-      {
-        tsconfig: "tsconfig.test.json",
-      },
-    ],
-    "^.+\\.(js|jsx)$": "babel-jest",
-  },
+	preset: "ts-jest",
+	moduleNameMapper: { "\\.(css|scss)$": "identity-obj-proxy" },
+	// to obtain access to the matchers.
+	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+	modulePaths: ["<rootDir>"],
+	testEnvironment: "jsdom",
+	transform: {
+		"^.+\\.(ts|tsx)$": [
+			"ts-jest",
+			{
+				tsconfig: "tsconfig.test.json",
+			},
+		],
+		"^.+\\.(js|jsx)$": "babel-jest",
+	},
 };
 
 export default config;

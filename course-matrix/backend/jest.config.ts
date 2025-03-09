b0587e55d5af1eede53fe15d1993eef9cb5cee5b
Austin-X <Austin-X@users.noreply.github.com>
2025-03-09 04:08:50 +0000

    Auto-formatted the code using Prettier

--- course-matrix/backend/jest.config.ts
+++ course-matrix/backend/jest.config.ts
@@ -1,21 +1,21 @@
 import type { Config } from "jest";
 
 const config: Config = {
-	preset: "ts-jest",
-	moduleNameMapper: { "\\.(css|scss)$": "identity-obj-proxy" },
-	// to obtain access to the matchers.
-	moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
-	modulePaths: ["<rootDir>"],
-	testEnvironment: "jsdom",
-	transform: {
-		"^.+\\.(ts|tsx)$": [
-			"ts-jest",
-			{
-				tsconfig: "tsconfig.test.json",
-			},
-		],
-		"^.+\\.(js|jsx)$": "babel-jest",
-	},
+  preset: "ts-jest",
+  moduleNameMapper: { "\\.(css|scss)$": "identity-obj-proxy" },
+  // to obtain access to the matchers.
+  moduleFileExtensions: ["ts", "tsx", "js", "jsx", "json", "node"],
+  modulePaths: ["<rootDir>"],
+  testEnvironment: "jsdom",
+  transform: {
+    "^.+\\.(ts|tsx)$": [
+      "ts-jest",
+      {
+        tsconfig: "tsconfig.test.json",
+      },
+    ],
+    "^.+\\.(js|jsx)$": "babel-jest",
+  },
 };
 
 export default config;

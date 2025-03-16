e8b4e3e7da0fac88e948479e977b42706a7d4e8f
dawangk <thomasyzy7@gmail.com>
2025-03-16 10:30:12 -0400

    Format

--- course-matrix/backend/src/controllers/generatorController.ts
+++ course-matrix/backend/src/controllers/generatorController.ts
@@ -1,14 +1,13 @@
-import { Request, Response } from "express";
+import {Request, Response} from 'express';
 
-import { supabase } from "../db/setupDb";
-import asyncHandler from "../middleware/asyncHandler";
+import {supabase} from '../db/setupDb';
+import asyncHandler from '../middleware/asyncHandler';
 
 async function getOfferings(course_id: number, semester: string) {
-  let { data: offeringData, error: offeringError } = await supabase
-    .schema("course")
-    .from("offerings")
-    .select(
-      `
+  let {data: offeringData, error: offeringError} = await supabase
+                                                       .schema('course')
+                                                       .from('offerings')
+                                                       .select(`
     id, 
     course_id, 
     meeting_section, 
@@ -24,38 +23,31 @@ async function getOfferings(course_id: number, semester: string) {
     instructor, 
     notes, 
     code
-  `,
-    )
-    .eq("course_id", course_id)
-    .eq("offering", semester);
-  console.log(course_id);
-  console.log(semester);
-  console.log(offeringData);
+  `).eq('course_id', course_id).eq('offering', semester);
 
   return offeringData;
 }
+
 
 export default {
   generateTimetable: asyncHandler(async (req: Request, res: Response) => {
     try {
       // Retrieve event properties from the request body
-      const { name, date, semester, search, courses, restrictions } = req.body;
+      const {name, date, semester, search, courses, restrictions} = req.body;
       const courseOfferingsList = [];
       // Retrieve the authenticated user
       const user_id = (req as any).user.id;
 
       // extracting offerings from each course
       for (const course of courses) {
-        const { id, code, name } = course;
-        courseOfferingsList.push({
-          course_id: id,
-          offerings: await getOfferings(id, semester),
-        });
+        const {id, code, name} = course;
+        courseOfferingsList.push(
+            {course_id: id, offerings: await getOfferings(id, semester)});
       }
 
       return res.status(200).json(courseOfferingsList);
     } catch (error) {
-      return res.status(500).send({ error });
+      return res.status(500).send({error});
     }
   }),
 };
--- course-matrix/backend/src/routes/courseRouter.ts
+++ course-matrix/backend/src/routes/courseRouter.ts
@@ -1,39 +1,33 @@
-import express from "express";
+import express from 'express';
 
-import coursesController from "../controllers/coursesController";
-import departmentsController from "../controllers/departmentsController";
-import generatorController from "../controllers/generatorController";
-import offeringsController from "../controllers/offeringsController";
-import { authHandler } from "../middleware/authHandler";
+import coursesController from '../controllers/coursesController';
+import departmentsController from '../controllers/departmentsController';
+import offeringsController from '../controllers/offeringsController';
+import {authHandler} from '../middleware/authHandler';
 
 export const coursesRouter = express.Router();
 export const departmentsRouter = express.Router();
 export const offeringsRouter = express.Router();
-export const generatorRouter = express.Router();
 
 /**
  * Route to get a list of courses.
  * @route GET /
  * @middleware authHandler - Middleware to check if the user is authenticated.
  */
-coursesRouter.get("/", authHandler, coursesController.getCourses);
+coursesRouter.get('/', authHandler, coursesController.getCourses);
 
 /**
  * Route to get a list of departments.
  * @route GET /
  * @middleware authHandler - Middleware to check if the user is authenticated.
  */
-departmentsRouter.get("/", authHandler, departmentsController.getDepartments);
+departmentsRouter.get('/', authHandler, departmentsController.getDepartments);
 
 /**
  * Route to get a list of offerings.
  * @route GET /
  * @middleware authHandler - Middleware to check if the user is authenticated.
  */
-offeringsRouter.get("/", authHandler, offeringsController.getOfferings);
+offeringsRouter.get('/', authHandler, offeringsController.getOfferings);
 
-generatorRouter.post(
-  "/generate",
-  authHandler,
-  generatorController.generateTimetable,
-);
+

dc10d320ee1bfed5d7ddf49acfc632ea5c2d3263
dawangk <dawangk@users.noreply.github.com>
2025-03-17 12:08:12 +0000

    Auto-formatted the code using Prettier

--- course-matrix/backend/src/controllers/generatorController.ts
+++ course-matrix/backend/src/controllers/generatorController.ts
@@ -1,7 +1,7 @@
-import {Request, Response} from 'express';
+import { Request, Response } from "express";
 
-import {supabase} from '../db/setupDb';
-import asyncHandler from '../middleware/asyncHandler';
+import { supabase } from "../db/setupDb";
+import asyncHandler from "../middleware/asyncHandler";
 
 export interface Offering {
   id: number;
@@ -25,28 +25,28 @@ export function createOffering(overrides: Partial<Offering> = {}): Offering {
   return {
     id: overrides.id ?? -1,
     course_id: overrides.course_id ?? -1,
-    meeting_section: overrides.meeting_section ?? 'No Section',
-    offering: overrides.offering ?? 'No Offering',
-    day: overrides.day ?? 'N/A',
-    start: overrides.start ?? '00:00:00',
-    end: overrides.end ?? '00:00:00',
-    location: overrides.location ?? 'No Room',
+    meeting_section: overrides.meeting_section ?? "No Section",
+    offering: overrides.offering ?? "No Offering",
+    day: overrides.day ?? "N/A",
+    start: overrides.start ?? "00:00:00",
+    end: overrides.end ?? "00:00:00",
+    location: overrides.location ?? "No Room",
     current: overrides.current ?? -1,
     max: overrides.max ?? -1,
     is_waitlisted: overrides.is_waitlisted ?? false,
-    delivery_mode: overrides.delivery_mode ?? 'N/A',
-    instructor: overrides.instructor ?? 'N/A',
-    notes: overrides.notes ?? 'N/A',
-    code: overrides.code ?? 'N/A',
+    delivery_mode: overrides.delivery_mode ?? "N/A",
+    instructor: overrides.instructor ?? "N/A",
+    notes: overrides.notes ?? "N/A",
+    code: overrides.code ?? "N/A",
   };
 }
 
 export enum RestrictionType {
-  RestrictBefore = 'Restrict Before',
-  RestrictAfter = 'Restrict After',
-  RestrictBetween = 'Restrict Between',
-  RestrictDay = 'Restrict Day',
-  RestrictDaysOff = 'Days Off'
+  RestrictBefore = "Restrict Before",
+  RestrictAfter = "Restrict After",
+  RestrictBetween = "Restrict Between",
+  RestrictDay = "Restrict Day",
+  RestrictDaysOff = "Days Off",
 }
 
 export interface Restriction {
@@ -63,10 +63,11 @@ export interface OfferingList {
 }
 
 export async function getOfferings(course_id: number, semester: string) {
-  let {data: offeringData, error: offeringError} = await supabase
-                                                       .schema('course')
-                                                       .from('offerings')
-                                                       .select(`
+  let { data: offeringData, error: offeringError } = await supabase
+    .schema("course")
+    .from("offerings")
+    .select(
+      `
     id, 
     course_id, 
     meeting_section, 
@@ -82,21 +83,25 @@ export async function getOfferings(course_id: number, semester: string) {
     instructor, 
     notes, 
     code
-  `).eq('course_id', course_id).eq('offering', semester);
+  `,
+    )
+    .eq("course_id", course_id)
+    .eq("offering", semester);
 
   return offeringData;
 }
 
-
-
-export const filterValidOfferings =
-    (offerings: Offering[], f: (x: Offering) => boolean):
-        Offering[] => {
-          return offerings.filter(f);
-        }
+export const filterValidOfferings = (
+  offerings: Offering[],
+  f: (x: Offering) => boolean,
+): Offering[] => {
+  return offerings.filter(f);
+};
 
 export function isValidOffering(
-    offering: Offering, restrictions: Restriction[]) {
+  offering: Offering,
+  restrictions: Restriction[],
+) {
   for (const restriction of restrictions) {
     if (restriction.disabled) continue;
 
@@ -110,8 +115,10 @@ export function isValidOffering(
         break;
 
       case RestrictionType.RestrictBetween:
-        if (offering.start >= restriction.startTime &&
-            offering.end <= restriction.endTime) {
+        if (
+          offering.start >= restriction.startTime &&
+          offering.end <= restriction.endTime
+        ) {
           return false;
         }
         break;
@@ -126,8 +133,12 @@ export function isValidOffering(
 }
 
 export async function getValidOfferings(
-    offerings: Offering[], restrictions: Restriction[]) {
-  return filterValidOfferings(offerings, x => isValidOffering(x, restrictions));
+  offerings: Offering[],
+  restrictions: Restriction[],
+) {
+  return filterValidOfferings(offerings, (x) =>
+    isValidOffering(x, restrictions),
+  );
 }
 
 export async function canInsert(toInsert: Offering, curList: Offering[]) {
@@ -143,39 +154,45 @@ export async function canInsert(toInsert: Offering, curList: Offering[]) {
 }
 
 export async function getValidSchedules(
-    courseOfferingsList: OfferingList[], curList: Offering[], cur: number,
-    len: number):
-    Promise<Offering[][]> {
-      if (cur == len) return [curList];
+  courseOfferingsList: OfferingList[],
+  curList: Offering[],
+  cur: number,
+  len: number,
+): Promise<Offering[][]> {
+  if (cur == len) return [curList];
 
-      const validSchedules: Offering[][] = [];
+  const validSchedules: Offering[][] = [];
 
-      const offeringsForCourse = courseOfferingsList[cur];
+  const offeringsForCourse = courseOfferingsList[cur];
 
-      for (const offering of offeringsForCourse.offerings) {
-        if (await canInsert(offering, curList)) {
-          curList.push(offering);
-          // Recursively call the function for the next course
-          const res: Offering[][] = await getValidSchedules(
-              courseOfferingsList, curList, cur + 1, len);
+  for (const offering of offeringsForCourse.offerings) {
+    if (await canInsert(offering, curList)) {
+      curList.push(offering);
+      // Recursively call the function for the next course
+      const res: Offering[][] = await getValidSchedules(
+        courseOfferingsList,
+        curList,
+        cur + 1,
+        len,
+      );
 
-          // If a valid schedule is found, return it
-          validSchedules.push(...res);
+      // If a valid schedule is found, return it
+      validSchedules.push(...res);
 
-          // If no valid schedule is found, pop the offering and continue
-          curList.pop();
-        }
-      }
-
-      // If no valid schedule is found for this course, return null
-      return validSchedules;
+      // If no valid schedule is found, pop the offering and continue
+      curList.pop();
     }
+  }
+
+  // If no valid schedule is found for this course, return null
+  return validSchedules;
+}
 
 export default {
   generateTimetable: asyncHandler(async (req: Request, res: Response) => {
     try {
       // Retrieve event properties from the request body
-      const {name, date, semester, search, courses, restrictions} = req.body;
+      const { name, date, semester, search, courses, restrictions } = req.body;
       const courseOfferingsList: OfferingList[] = [];
       const validCourseOfferingsList: OfferingList[] = [];
       // Retrieve the authenticated user
@@ -183,30 +200,37 @@ export default {
 
       // extracting offerings from each course
       for (const course of courses) {
-        const {id, code, name} = course;
-        courseOfferingsList.push(
-            {course_id: id, offerings: await getOfferings(id, semester) ?? []});
+        const { id, code, name } = course;
+        courseOfferingsList.push({
+          course_id: id,
+          offerings: (await getOfferings(id, semester)) ?? [],
+        });
       }
-      courseOfferingsList.forEach(
-          course => console.log(JSON.stringify(course, null, 2)));
+      courseOfferingsList.forEach((course) =>
+        console.log(JSON.stringify(course, null, 2)),
+      );
 
       // filter out invalid course entries
-      for (const {course_id, offerings} of courseOfferingsList) {
+      for (const { course_id, offerings } of courseOfferingsList) {
         validCourseOfferingsList.push({
           course_id: course_id,
-          offerings: await getValidOfferings(offerings ?? [], restrictions)
+          offerings: await getValidOfferings(offerings ?? [], restrictions),
         });
       }
 
       const validSchedules = await getValidSchedules(
-          validCourseOfferingsList, [], 0, validCourseOfferingsList.length);
+        validCourseOfferingsList,
+        [],
+        0,
+        validCourseOfferingsList.length,
+      );
 
       if (validSchedules.length === 0) {
-        return res.status(404).json({error: 'No valid schedules found.'});
+        return res.status(404).json({ error: "No valid schedules found." });
       }
-      return res.status(200).json({validSchedules});
+      return res.status(200).json({ validSchedules });
     } catch (error) {
-      return res.status(500).send({error});
+      return res.status(500).send({ error });
     }
   }),
 };
--- course-matrix/backend/src/routes/courseRouter.ts
+++ course-matrix/backend/src/routes/courseRouter.ts
@@ -1,9 +1,9 @@
-import express from 'express';
+import express from "express";
 
-import coursesController from '../controllers/coursesController';
-import departmentsController from '../controllers/departmentsController';
-import offeringsController from '../controllers/offeringsController';
-import {authHandler} from '../middleware/authHandler';
+import coursesController from "../controllers/coursesController";
+import departmentsController from "../controllers/departmentsController";
+import offeringsController from "../controllers/offeringsController";
+import { authHandler } from "../middleware/authHandler";
 
 export const coursesRouter = express.Router();
 export const departmentsRouter = express.Router();
@@ -14,20 +14,18 @@ export const offeringsRouter = express.Router();
  * @route GET /
  * @middleware authHandler - Middleware to check if the user is authenticated.
  */
-coursesRouter.get('/', authHandler, coursesController.getCourses);
+coursesRouter.get("/", authHandler, coursesController.getCourses);
 
 /**
  * Route to get a list of departments.
  * @route GET /
  * @middleware authHandler - Middleware to check if the user is authenticated.
  */
-departmentsRouter.get('/', authHandler, departmentsController.getDepartments);
+departmentsRouter.get("/", authHandler, departmentsController.getDepartments);
 
 /**
  * Route to get a list of offerings.
  * @route GET /
  * @middleware authHandler - Middleware to check if the user is authenticated.
  */
-offeringsRouter.get('/', authHandler, offeringsController.getOfferings);
-
-
+offeringsRouter.get("/", authHandler, offeringsController.getOfferings);

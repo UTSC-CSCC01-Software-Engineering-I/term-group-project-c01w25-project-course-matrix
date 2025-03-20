ed6657150a2d21f62625bc4e269c3606bc063377
FLAME <xaustin03@gmail.com>
2025-03-20 01:49:34 -0400

    Fix Winter 2026 and Fall 2025 semester start date and end date

--- course-matrix/frontend/src/utils/semester-utils.ts
+++ course-matrix/frontend/src/utils/semester-utils.ts
@@ -4,13 +4,13 @@ export function getSemesterStartAndEndDates(semester: string) {
       semester === "Summer 2025"
         ? "2025-05-02"
         : semester === "Fall 2025"
-          ? "2025-09-03"
-          : "2026-01-06",
+          ? "2025-09-02"
+          : "2026-01-05",
     end:
       semester === "Summer 2025"
         ? "2025-08-07"
         : semester === "Fall 2025"
-          ? "2025-12-03"
-          : "2026-04-04",
+          ? "2025-12-02"
+          : "2026-04-06",
   };
 }

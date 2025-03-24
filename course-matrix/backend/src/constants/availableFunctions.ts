249cd73a4c41246b968e379e246061962ece5d2c
dawangk <dawangk@users.noreply.github.com>
2025-03-24 16:28:12 +0000

    Auto-formatted the code using Prettier

--- course-matrix/backend/src/constants/availableFunctions.ts
+++ course-matrix/backend/src/constants/availableFunctions.ts
@@ -195,8 +195,11 @@ export const availableFunctions: AvailableFunctions = {
     try {
       // Extract event details and course information from the request
       const { name, semester, courses, restrictions } = args;
-      if(name.length>=50){
-        return {status: 400, error: "timetable title is over 50 characters long"};
+      if (name.length >= 50) {
+        return {
+          status: 400,
+          error: "timetable title is over 50 characters long",
+        };
       }
 
       const courseOfferingsList: OfferingList[] = [];
